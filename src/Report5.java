3f57221efc31ccc28426a285be8d2af4c06b9bd4
Karen Hguchi <e245747@ie.u-ryukyu.ac.jp>
2024-12-16 12:25:26 +0900

    step2

--- src/Report5.java
+++ src/Report5.java
@@ -2,8 +2,14 @@ public class Report5 {
     public static void main(String[] args) {
         // 条件1: String型変数strを宣言し、nullを代入
         String str = null;
-
+    try{
         // 条件2: str.length() を呼び出す
         System.out.println(str.length());
+    }catch(NullPointerException e){
+        System.out.println("NullPointerExceptionが発生");
+        System.out.println(e.getMessage());
+    }
     }
 }
+    
+
