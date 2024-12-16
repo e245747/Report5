335ce223ebe9c742474a841e18dea9d6d79a8865
Karen Hguchi <e245747@ie.u-ryukyu.ac.jp>
2024-12-16 12:07:32 +0900

    step1

---
+++ src/Report5.java
@@ -0,0 +1,9 @@
+public class Report5 {
+    public static void main(String[] args) {
+        // 条件1: String型変数strを宣言し、nullを代入
+        String str = null;
+
+        // 条件2: str.length() を呼び出す
+        System.out.println(str.length());
+    }
+}
